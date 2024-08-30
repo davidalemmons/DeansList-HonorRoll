d8c74f7546dc9aca8e3d81903101104352a6e8d6
davidalemmons <109745379+davidalemmons@users.noreply.github.com>
2024-08-30 15:14:09 -0400

    Add files via upload

---
+++ DeansList-HonorRoll.py
@@ -0,0 +1,31 @@
+#Author: David Lemmons
+#File Name: DeansList-HonorRoll.py
+#Description:
+#This application will allow for the user to input a student's last name or "zzz" to exit the program. If "zzz" is not entered, the program
+#will continue to ask the student's first name and GPA. The program then takes the student's GPA and determines whether or not the student
+#made the Dean's List, Honor Roll, or needs to study more. Then the program repeats until the user exits by entering zzz as the student's
+#last name. The "study more" response was not requested for this app but was added as a response to handle GPAs under 3.25
+
+
+
+def qualify():
+
+    while True:
+        lname = input("Enter the student's last name or 'zzz' to quit: ")
+
+        if lname =='zzz':
+            print("Goodbye!")
+            break
+
+        fname = input("Enter student's first name: ")
+
+        gpa = float(input("Enter student's GPA: "))
+
+        if gpa >= 3.5:
+            print(fname+ " "+lname+ " has made the Dean's List!")
+        elif gpa >= 3.25:
+            print(fname+ " "+lname+ " has made the Honor Roll!")
+        else:
+            print(fname+ " "+lname+ " should study more and has not made the Honor Roll or Dean's List")
+
+qualify()
